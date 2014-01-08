a4d763c1f2b9ea0910013eb29215165dc01964c1
Michitaro Naito <m-naito@amlitek.com>
2014-01-09 00:57:13 +0900

    Add: SignalR Self Host

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.Owin.Cors;
 
 namespace GitTest
 {
@@ -10,9 +10,31 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, GIT!");
-            Console.WriteLine("Hello again, GIT!");
-            Console.Read();
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            string url = "http://localhost:8080";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
         }
     }
 }
