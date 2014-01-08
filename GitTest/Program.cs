3487369a4c93875ef3f1e4963f27b5250bdc0e01
Michitaro Naito <m-naito@amlitek.com>
2014-01-09 00:41:51 +0900

    Add: Hello again

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, GIT!");
+            Console.WriteLine("Hello again, GIT!");
             Console.Read();
         }
     }
